4dece3da7f62608b6592c21a94e784e60701cae3
stefkovicsrichard <stefkovics.richard@ckik.hu>
2025-05-29 11:30:18 +0200

    Add project files.

---
+++ Speedrun.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace speedruns
+{
+    internal class Speedrun
+    {
+        private string runner;
+        private int place;
+        private float time;
+        private string cat;
+
+        public Speedrun(string runner, int place, float time, string cat)
+        {
+            this.runner = runner;
+            this.place = place;
+            this.time = time;
+            this.cat = cat;
+        }
+
+        public Speedrun(string runner, int place, string time, string cat)
+        {
+            this.runner = runner;
+            this.place = place;
+            float ts = Convert.ToSingle(Math.Round(TimeSpan.Parse(time).TotalSeconds, 2));
+            this.time = ts;
+            this.cat = cat;
+        }
+
+        public float Time
+        {
+            get { return this.time; }
+        }
+    }
+}
---
+++ Speedruns.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace speedruns
+{
+    internal class Speedruns
+    {
+        private string game;
+        private float wr;
+        private List<Speedrun> runs;
+
+        public Speedruns(string game, List<Speedrun> runs)
+        {
+            this.game = game;
+            this.runs = runs;
+            this.wr = runs[0].Time;
+        }
+
+        public Speedruns(string path)
+        {
+            StreamReader sr = new StreamReader(path);
+            this.game = sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                string[] line = sr.ReadLine().Split(';');
+                string runner = line[0];
+                int place = int.Parse(line[1]);
+                //float time = TimeSpan.TryParse();
+            }
+        }
+    }
+}
